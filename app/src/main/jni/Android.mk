cf19d43bb72855f4c931d5c5211c6521db6c264e
Fredrik Fornwall <fredrik@fornwall.net>
2016-05-20 10:30:25 +0200

    Gradle build updates
    - Switch to using gradle to build jni lib.
    - Enable proguard minification.
    - Add the Android support library.

--- app/src/main/jni/Android.mk
+++
@@ -1,5 +0,0 @@
-LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_MODULE:= libtermux
-LOCAL_SRC_FILES:= termux.c
-include $(BUILD_SHARED_LIBRARY)
--- app/src/main/jni/Application.mk
+++
@@ -1,5 +0,0 @@
-APP_ABI := arm64-v8a armeabi-v7a x86
-APP_PLATFORM := android-21
-NDK_TOOLCHAIN_VERSION := 4.9
-APP_CFLAGS := -std=c11 -Wall -Wextra -Os -fno-stack-protector
-APP_LDFLAGS = -nostdlib -Wl,--gc-sections
