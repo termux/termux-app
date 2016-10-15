8ad7a6669c0c4301266c44da5dfde65f0ba16546
Fredrik Fornwall <fredrik@fornwall.net>
2016-10-16 00:13:35 +0200

    Switch a commit() to apply() for shared prefs

--- app/src/main/java/com/termux/app/TermuxPreferences.java
+++ app/src/main/java/com/termux/app/TermuxPreferences.java
@@ -105,7 +105,7 @@ final class TermuxPreferences {
     }
 
     static void storeCurrentSession(Context context, TerminalSession session) {
-        PreferenceManager.getDefaultSharedPreferences(context).edit().putString(TermuxPreferences.CURRENT_SESSION_KEY, session.mHandle).commit();
+        PreferenceManager.getDefaultSharedPreferences(context).edit().putString(TermuxPreferences.CURRENT_SESSION_KEY, session.mHandle).apply();
     }
 
     static TerminalSession getCurrentSession(TermuxActivity context) {
