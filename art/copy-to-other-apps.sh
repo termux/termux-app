e3a1f8224f76ba9bfc4a3116f8884c6fb65152d7
Fredrik Fornwall <fredrik@fornwall.net>
2016-12-26 04:45:00 +0100

    Add art/copy-to-other-apps.sh script

---
+++ art/copy-to-other-apps.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+for DENSITY in mdpi hdpi xhdpi xxhdpi xxxhdpi; do
+	FOLDER=../app/src/main/res/mipmap-$DENSITY
+
+	for FILE in ic_launcher ic_launcher_round; do
+		PNG=$FOLDER/$FILE.png
+
+		# Update other apps:
+		for APP in tasker widget api; do
+			APPDIR=../../termux-$APP
+			if [ -d $APPDIR ]; then
+				APP_FOLDER=$APPDIR/app/src/main/res/mipmap-$DENSITY
+				mkdir -p $APP_FOLDER
+				cp $PNG $APP_FOLDER/$FILE.png
+			fi
+		done
+	done
+
+done
