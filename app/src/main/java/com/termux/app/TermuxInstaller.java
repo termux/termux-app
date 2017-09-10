45d815ae39f36400ac303a7d3e3e06e3f7835ce6
thewisenerd <thewisenerd@protonmail.com>
2017-09-10 08:19:57 -0400

    termux: app: add apps-cache intent
    write all launchable activities into $HOME/.apps
    
    (cherry picked from commit b877a81b999179af2d702b8929b6d20e29dde054)

--- app/src/main/java/com/termux/app/TermuxInstaller.java
+++ app/src/main/java/com/termux/app/TermuxInstaller.java
@@ -4,6 +4,13 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.DialogInterface.OnDismissListener;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Environment;
 import android.os.UserManager;
 import android.system.Os;
@@ -20,6 +27,9 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
@@ -243,5 +253,47 @@ final class TermuxInstaller {
             }
         }.start();
     }
+
+    public static void setupAppListCache(final Context context) {
+        final String LOG_TAG = "termux-applist";
+        final String APPLIST_CACHE_FILE = ".apps";
+        new Thread() {
+            public void run() {
+                try {
+
+                    final File targetFile = new File(TermuxService.HOME_PATH, APPLIST_CACHE_FILE);
+                    final FileOutputStream outStream = new FileOutputStream(targetFile);
+                    final PrintStream printStream = new PrintStream(outStream);
+
+                    final PackageManager pm = context.getPackageManager();
+                    List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+                    for (ApplicationInfo packageInfo : packages) {
+                        final String  packageName     = packageInfo.packageName;
+                        final String  appName         = packageInfo.loadLabel(pm).toString();
+                        final String  sourceDir       = packageInfo.sourceDir;
+                        final Intent  LaunchActivity  = pm.getLaunchIntentForPackage(packageName);
+                        final Boolean isSystemApp     = ((packageInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 1) ? true : false;
+
+                        Log.d(LOG_TAG, "[" + LaunchActivity + "] : [" + packageName + "] : [" + isSystemApp + "] : [" + appName + "]");
+                        if (LaunchActivity == null) {
+                            continue;
+                        }
+
+                        final String  LaunchComponent = LaunchActivity.getComponent().flattenToShortString();
+                        printStream.print( appName + "|" + LaunchComponent + "|" + packageName + "|" + isSystemApp + "\n");
+                    } // for package in packages
+
+                    // close file
+                    printStream.flush();
+                    printStream.close();
+                    outStream.flush();
+                    outStream.close();
+
+                } catch (Exception e) {
+                    Log.e(LOG_TAG, "Error setting up applist-cache", e);
+                }
+            }
+        }.start();
+    };
 
 }
