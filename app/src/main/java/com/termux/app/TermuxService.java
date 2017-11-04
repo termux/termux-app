694ccc38c41b47fd7fa17b94ec9f10febde15bb4
Fredrik Fornwall <fredrik@fornwall.net>
2017-11-04 23:38:49 +0100

    Remove WakefulBroadcastReceiver usage

--- app/src/main/java/com/termux/app/TermuxService.java
+++ app/src/main/java/com/termux/app/TermuxService.java
@@ -16,7 +16,6 @@ import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.PowerManager;
-import android.support.v4.content.WakefulBroadcastReceiver;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 
@@ -155,11 +154,6 @@ public final class TermuxService extends Service implements SessionChangedCallba
             }
         } else if (action != null) {
             Log.e(EmulatorDebug.LOG_TAG, "Unknown TermuxService action: '" + action + "'");
-        }
-
-        if ((flags & START_FLAG_REDELIVERY) == 0) {
-            // Service is started by WBR, not restarted by system, so release the WakeLock from WBR.
-            WakefulBroadcastReceiver.completeWakefulIntent(intent);
         }
 
         // If this service really do get killed, there is no point restarting it automatically - let the user do on next
