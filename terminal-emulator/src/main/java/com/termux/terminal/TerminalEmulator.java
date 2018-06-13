060595f8ed605fa41d5f2c1babc9dc5c590309ac
easyaspi314 (Devin) <easyaspi314@users.noreply.github.com>
2018-06-12 23:49:51 -0400

    Replace CRLF with CR as well.
    This should replace both \r\n and \n with \r now.
    
    "\r?\n" matches 0 or 1 \r and one \n, which should capture both
    escape sequences.

--- terminal-emulator/src/main/java/com/termux/terminal/TerminalEmulator.java
+++ terminal-emulator/src/main/java/com/termux/terminal/TerminalEmulator.java
@@ -2338,8 +2338,8 @@ public final class TerminalEmulator {
     public void paste(String text) {
         // First: Always remove escape key and C1 control characters [0x80,0x9F]:
         text = text.replaceAll("(\u001B|[\u0080-\u009F])", "");
-        // Second: Replace all newlines (\n) with carriage returns (\r).
-        text = text.replace('\n', '\r');
+        // Second: Replace all newlines (\n) or CRLF (\r\n) with carriage returns (\r).
+        text = text.replaceAll("\r?\n", "\r");
 
         // Then: Implement bracketed paste mode if enabled:
         boolean bracketed = isDecsetInternalBitSet(DECSET_BIT_BRACKETED_PASTE_MODE);
