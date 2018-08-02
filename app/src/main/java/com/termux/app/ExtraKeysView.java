7bd18c420c469cbe3fb864df9d4710642511d86a
Robert Vanden Eynde <robertvandeneynde@hotmail.com>
2018-08-02 16:34:43 +0200

    Add aliases for BACKSLASH QUOTE and APOSTROPHE

--- app/src/main/java/com/termux/app/ExtraKeysView.java
+++ app/src/main/java/com/termux/app/ExtraKeysView.java
@@ -257,7 +257,7 @@ public final class ExtraKeysView extends GridLayout {
         put("LT", "LEFT"); 
         put("RT", "RIGHT");
         put("DN", "DOWN");
-        // put("UP", "UP"); well, the direction is already two letters
+        // put("UP", "UP"); well, "UP" is already two letters
         
         put("PAGEUP", "PGUP");
         put("PAGE_UP", "PGUP");
@@ -274,6 +274,11 @@ public final class ExtraKeysView extends GridLayout {
         
         put("DELETE", "DEL");
         put("BACKSPACE", "BKSP");
+        
+        // easier for writing in termux.properties
+        put("BACKSLASH", "\\");
+        put("QUOTE", "\"");
+        put("APOSTROPHE", "'");
     }};
     
     /**
