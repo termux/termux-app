07d6c9bd5ec1360911b44b47e05cc40f3d2890a8
Fredrik Fornwall <fredrik@fornwall.net>
2018-08-28 02:51:11 +0200

    Remove unused import

--- app/src/main/java/com/termux/app/TermuxActivity.java
+++ app/src/main/java/com/termux/app/TermuxActivity.java
@@ -69,7 +69,6 @@ import com.termux.view.TerminalView;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Collections;
