78ff948383c45450777e1009e6889af6f31eb167
iamahuman <iamahuman@users.noreply.github.com>
2019-01-25 12:31:35 +0900

    Implement CSI 3 J - Clear transcript

--- terminal-view/src/main/java/com/termux/view/TerminalView.java
+++ terminal-view/src/main/java/com/termux/view/TerminalView.java
@@ -358,10 +358,12 @@ public final class TerminalView extends View {
     public void onScreenUpdated() {
         if (mEmulator == null) return;
 
+        int rowsInHistory = mEmulator.getScreen().getActiveTranscriptRows();
+        if (mTopRow < -rowsInHistory) mTopRow = -rowsInHistory;
+
         boolean skipScrolling = false;
         if (mIsSelectingText) {
             // Do not scroll when selecting text.
-            int rowsInHistory = mEmulator.getScreen().getActiveTranscriptRows();
             int rowShift = mEmulator.getScrollCounter();
             if (-mTopRow + rowShift > rowsInHistory) {
                 // .. unless we're hitting the end of history transcript, in which
