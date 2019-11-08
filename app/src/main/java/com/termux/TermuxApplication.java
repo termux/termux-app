7a64150cae936dfb58befc348a16bf0678e31311
Neo <c.min.tseng@gmail.com>
2019-11-08 14:58:36 +0800

    Refactoring -- make Folder & PREFIX Config public

--- app/src/main/java/com/termux/TermuxApplication.java
+++ app/src/main/java/com/termux/TermuxApplication.java
@@ -4,6 +4,10 @@ import android.app.Application;
 import android.content.Context;
 import android.util.Log;
 
+import com.termux.service.TermuxConfig;
+
+import java.io.File;
+
 public class TermuxApplication extends Application {
     private static final String TAG = "TermuxApplication";
     private static TermuxApplication sApplication;
@@ -18,5 +22,9 @@ public class TermuxApplication extends Application {
     public void onCreate() {
         super.onCreate();
         sApplication = this;
+        final File rootDataDir = sApplication.getFilesDir();
+        TermuxConfig.FILES_PATH = rootDataDir.toString();
+        TermuxConfig.PREFIX_PATH = TermuxConfig.FILES_PATH + "/usr";
+        TermuxConfig.HOME_PATH = TermuxConfig.FILES_PATH + "/home";
     }
 }
--- app/src/main/java/com/termux/service/TermuxConfig.java
+++ app/src/main/java/com/termux/service/TermuxConfig.java
@@ -8,7 +8,7 @@ public final class TermuxConfig {
      * Note that this is a symlink on the Android M preview.
      */
     @SuppressLint("SdCardPath")
-    public final static String FILES_PATH = "/data/data/com.termux/files";
-    public final static String PREFIX_PATH = FILES_PATH + "/usr";
-    public final static String HOME_PATH = FILES_PATH + "/home";
+    public static String FILES_PATH = "/data/data/com.termux/files";
+    public static String PREFIX_PATH = FILES_PATH + "/usr";
+    public static String HOME_PATH = FILES_PATH + "/home";
 }
