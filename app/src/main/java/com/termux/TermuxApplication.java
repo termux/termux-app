e520c04d7f8174d26e5e8915e22befba3867004a
Neo <c.min.tseng@gmail.com>
2019-11-08 20:42:14 +0800

    Refactoring -- add TermuxServiceApplication

--- app/src/main/java/com/termux/TermuxApplication.java
+++ app/src/main/java/com/termux/TermuxApplication.java
@@ -1,30 +1,18 @@
 package com.termux;
 
-import android.app.Application;
 import android.content.Context;
-import android.util.Log;
 
-import com.termux.service.TermuxConfig;
-
-import java.io.File;
-
-public class TermuxApplication extends Application {
+public class TermuxApplication extends TermuxServiceApplication {
     private static final String TAG = "TermuxApplication";
-    private static TermuxApplication sApplication;
 
     @Override
     protected void attachBaseContext(Context context) {
         super.attachBaseContext(context);
-        Log.e(TAG, "Show Application attachBaseContext");
+
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
-        sApplication = this;
-        final File rootDataDir = sApplication.getFilesDir();
-        TermuxConfig.FILES_PATH = rootDataDir.toString();
-        TermuxConfig.PREFIX_PATH = TermuxConfig.FILES_PATH + "/usr";
-        TermuxConfig.HOME_PATH = TermuxConfig.FILES_PATH + "/home";
     }
 }
---
+++ terminal-service/src/main/java/com/termux/TermuxServiceApplication.java
@@ -0,0 +1,30 @@
+package com.termux;
+
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
+import com.termux.service.TermuxConfig;
+
+import java.io.File;
+
+public abstract class TermuxServiceApplication extends Application {
+    private static final String TAG = "TermuxServiceApplication";
+    private static TermuxServiceApplication sApplication;
+
+    @Override
+    protected void attachBaseContext(Context context) {
+        super.attachBaseContext(context);
+        Log.e(TAG, "Show Application attachBaseContext");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sApplication = this;
+        final File rootDataDir = sApplication.getFilesDir();
+        TermuxConfig.FILES_PATH = rootDataDir.toString();
+        TermuxConfig.PREFIX_PATH = TermuxConfig.FILES_PATH + "/usr";
+        TermuxConfig.HOME_PATH = TermuxConfig.FILES_PATH + "/home";
+    }
+}
