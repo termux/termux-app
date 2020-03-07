eddd6906418f6e1e99470dcd1e11d41f47bb3f72
v1nc <github@reckendrees.systems>
2020-03-08 00:52:50 +0100

    update setup and motd

--- bootstrap/changes/tel/setup.sh
+++ bootstrap/changes/tel/setup.sh
@@ -11,7 +11,7 @@ echo "[TEL]: installing required packages:"
 pkg install fzf byobu curl tmux zsh ncurses-utils git make -y
 
 echo "[TEL]: installing app launcher:"
-mkdir -p ~/.local/share/termux-launcher-app-cache
+mkdir -p ~/.local/share/
 git clone https://github.com/Neo-Oli/termux-app-launcher
 cd termux-app-launcher
 make install
@@ -21,8 +21,9 @@ app -u
 
 echo "[TEL]: installing OhMyZsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 mv ~/../usr/etc/motd_finished ~/../usr/etc/motd
 echo "cat ~/../usr/etc/motd" >> .zshrc
 sed -i 's/robbyrussell/avit/g' .zshrc
 echo "[TEL]: final step: type 'byobu-enable'"
-echo "[TEL]: after that, type 'exit' and restart app!"
+echo "[TEL]: after that, type 'exit' and restart the app!"
