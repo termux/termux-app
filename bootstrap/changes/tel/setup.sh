2d42c7a16eb86dfcd158e5db108439465c4ed899
v1nc <github@reckendrees.systems>
2020-03-08 22:36:26 +0100

    add byobu colors, activate scrolling mode(thanks to the dino)

--- bootstrap/changes/tel/setup.sh
+++ bootstrap/changes/tel/setup.sh
@@ -20,10 +20,12 @@ rm -rf termux-app-launcher
 app -u
 
 echo "[TEL]: installing OhMyZsh"
+echo "if you enable zsh, hit 'exit' to finish setup."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 mv ~/../usr/etc/motd_finished ~/../usr/etc/motd
 echo "cat ~/../usr/etc/motd" >> .zshrc
 sed -i 's/robbyrussell/avit/g' .zshrc
-echo "[TEL]: final step: type 'byobu-enable'"
-echo "[TEL]: after that, type 'exit' and restart the app!"
+echo "[TEL]: final step: hit 'byobu-enable'"
+echo "[TEL]: after that, hit 'exit' and restart the app!"
+echo "[TEL]: if the layout is still missing, hit 'byobu-enable' and 'exit' again!"
