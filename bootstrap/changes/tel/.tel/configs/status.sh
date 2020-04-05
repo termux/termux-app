0ba6a77b9c213e43784bdfee214f4a00b6a1d06d
v1nc <github@reckendrees.systems>
2020-04-05 15:21:09 +0200

    change status reload to 2

--- bootstrap/changes/tel/.tel/configs/status.sh
+++ bootstrap/changes/tel/.tel/configs/status.sh
@@ -5,7 +5,7 @@
 export STATUS_ENABLED=true #status window on startup?
 export STATUS_CENTER=true #center status output [true/false]
 export STATUS_SPACING_CHAR=" " #char to create spacing if center is enabled, try "="
-export STATUS_RELOAD=4 #time to sleep before reloading status
+export STATUS_RELOAD=2 #time to sleep before reloading status
 export STATUS_COLOR=false #enable status color via lolcat [true/false]
 export STATUS_COLOR_SPREAD="100" #lolcat color spread value
 export NOTIFICATIONS=true #enable notification display in status
