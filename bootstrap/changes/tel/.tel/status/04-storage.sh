a727c83030bd233f6d0869ced7d18756ed1a0372
SealedJoy <sealedjoy@gmail.com>
2020-04-09 18:16:21 +0100

    more storage changes

--- bootstrap/changes/tel/.tel/status/04-storage.sh
+++ bootstrap/changes/tel/.tel/status/04-storage.sh
@@ -2,8 +2,8 @@
 # Basic Storage script for TEL
 # SealyJ
 cmd=$(df -h ~/storage)
-intfree=$(echo $cmd | grep data/media | rev | cut -d " " -f4 | rev)
-drive=$(echo $cmd | grep data/media | rev | cut -d " " -f1 | rev)
+intfree=$(echo $cmd | grep G | rev | cut -d " " -f4 | rev)
+drive=$(echo $cmd | grep G | rev | cut -d " " -f1 | rev)
 #percused=$(echo cmd | grep data/media | rev | cut -d " " -f2 | rev)
 #external=
 echo " $intfree free on $drive"
