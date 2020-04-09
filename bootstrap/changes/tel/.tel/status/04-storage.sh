2a36c5d0a0f7f6e2f529e35fd6d7178804773c65
SealedJoy <sealedjoy@gmail.com>
2020-04-09 15:36:06 +0100

    update storage status script to use symlinked path

--- bootstrap/changes/tel/.tel/status/04-storage.sh
+++ bootstrap/changes/tel/.tel/status/04-storage.sh
@@ -1,8 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/bash
 # Basic Storage script for TEL
 # SealyJ
-intfree=$(df -h | grep data/media | rev | cut -d " " -f4 | rev)
-drive=$(df -h | grep data/media | rev | cut -d " " -f1 | rev)
-percused=$(df -h | grep data/media | rev | cut -d " " -f2 | rev)
+cmd=$(df -h ~/storage)
+intfree=$(echo cmd | grep data/media | rev | cut -d " " -f4 | rev)
+drive=$(echo cmd | grep data/media | rev | cut -d " " -f1 | rev)
+#percused=$(echo cmd | grep data/media | rev | cut -d " " -f2 | rev)
 #external=
 echo " $intfree free on $drive"
