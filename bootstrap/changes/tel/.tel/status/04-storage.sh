bcb5dccb2d882d1c669b6784e82c4430cce12a0c
SealedJoy <sealedjoy@gmail.com>
2020-04-09 20:48:11 +0100

    storage.sh changes

--- bootstrap/changes/tel/.tel/status/04-storage.sh
+++ bootstrap/changes/tel/.tel/status/04-storage.sh
@@ -3,12 +3,11 @@
 
 errormsg="~/storage not found, please run termux-setup-storage."
 cmd=$(df -h ~/storage || echo $errormsg)
-if [ $cmd == $errormsg ]; then
-       	echo $errormsg
+if [ "$cmd" == "$errormsg" ]; then
+       	echo "$errormsg"
 else
 intfree=$(echo $cmd | grep G | rev | cut -d " " -f4 | rev)
-drive=$(echo $cmd | grep G | rev | cut -d " " -f1 | rev)
+#drive=$(echo $cmd | grep G | rev | cut -d " " -f1 | rev)
 #percused=$(echo cmd | grep data/media | rev | cut -d " " -f2 | rev)
-#external=
-echo " $intfree free on $drive"
+echo " $intfree free on ~/storage"
 fi
