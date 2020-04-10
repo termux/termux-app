d70d64a4e01c7fc1fd441a9611be7f2e8ddc4c25
SealedJoy <sealedjoy@gmail.com>
2020-04-10 03:45:29 +0100

    default config

--- bootstrap/changes/tel/.tel/configs/status.sh
+++ bootstrap/changes/tel/.tel/configs/status.sh
@@ -7,8 +7,8 @@
 export STATUS_ENABLED=true #status pane on startup?
 export STATUS_CENTER=true #center output in status pane [true/false]
 export STATUS_SPACING_CHAR=" " #char to create spacing if center is enabled, try "="
-export STATUS_RELOAD=1 #time to sleep betweem status pane refresh (higher may save battery) [seconds]
-export STATUS_TIMEOUT=6 #timeout for status scripts (if status not fully loading try increasing) [seconds]
+export STATUS_RELOAD=2 #time to sleep betweem status pane refresh (higher may save battery) [seconds]
+export STATUS_TIMEOUT=4 #timeout for status scripts (if status not fully loading try increasing) [seconds]
 export STATUS_COLOR=false #enable status color via lolcat [true/false]
 export STATUS_COLOR_SPREAD="150" #lolcat color spread value
 export NOTIFICATIONS=true #enable notification display in status
