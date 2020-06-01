b6c5854cce33933c2a9f27a3d8c4d5b80cf52485
purofle <3272912942@qq.com>
2020-06-01 23:52:07 +0800

    activity test

--- app/src/main/java/com/termux/app/TermuxActivity.java
+++ app/src/main/java/com/termux/app/TermuxActivity.java
@@ -862,7 +862,7 @@ public final class TermuxActivity extends Activity implements ServiceConnection 
                 return true;
             }
             case CONTEXTMENU_TEST:
-                showToast("test", true);
+                startActivity(new Intent(this, TermuxFunctionActivity.class))
                 return true;
             case CONTEXTMENU_HELP_ID:
                 startActivity(new Intent(this, TermuxHelpActivity.class));
---
+++ app/src/main/java/com/termux/app/TermuxFunctionActivity.java
@@ -0,0 +1,11 @@
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public final class TermuxFunctionActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.function);
+    }
+}
