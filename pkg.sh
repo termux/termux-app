d4e604214e93ded9974613fa5223358fd9d9f7f4
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-08-17 00:02:16 +0300

    pkg.sh: reformat with tabs, update usage info, don't use shortcuts

--- pkg.sh
+++ pkg.sh
@@ -8,56 +8,61 @@ set -e -u
 # adb shell am startservice --user 0 -a com.termux.list_packages com.termux/com.termux.app.TermuxService
 
 show_help() {
-    echo 'Usage: pkg command [arguments]'
-    echo ''
-    echo 'A tool for managing packages. Commands:'
-    echo ''
-    echo '  install <packages>'
-    exit 1
+	echo 'Usage: pkg command [arguments]'
+	echo
+	echo 'A tool for managing packages. Commands:'
+	echo
+	echo '  add    <packages>   - Install specified packages'
+	echo '  remove <packages>   - Uninstall specified packages'
+	echo '  list                - List installed packages'
+	echo
 }
 
 if [ $# = 0 ]; then
-    show_help
+	show_help
+	exit 1
 fi
 
 CMD="$1"
 shift 1
 
 install_packages() {
-    ALL_PACKAGES="$@"
-    am startservice \
-        --user 0 \
-        --esa packages "${ALL_PACKAGES// /,}" \
-        -a com.termux.install_packages \
-        com.termux/com.termux.app.TermuxService \
-        >/dev/null
+	local all_packages="$*"
+
+	am startservice \
+		--user 0 \
+		--esa packages "${all_packages// /,}" \
+		-a com.termux.install_packages \
+		com.termux/com.termux.app.TermuxService \
+		>/dev/null
 }
 
 uninstall_packages() {
-    ALL_PACKAGES="$@"
-    am startservice \
-        --user 0 \
-        --esa packages "${ALL_PACKAGES// /,}" \
-        -a com.termux.uninstall_packages \
-        com.termux/com.termux.app.TermuxService \
-        >/dev/null
+	local all_packages="$*"
+
+	am startservice \
+		--user 0 \
+		--esa packages "${all_packages// /,}" \
+		-a com.termux.uninstall_packages \
+		com.termux/com.termux.app.TermuxService \
+		>/dev/null
 }
 
 list_packages() {
-    am startservice \
-        --user 0 \
-        -a com.termux.list_packages \
-        com.termux/com.termux.app.TermuxService \
-        >/dev/null
+	am startservice \
+		--user 0 \
+		-a com.termux.list_packages \
+		com.termux/com.termux.app.TermuxService \
+		>/dev/null
 }
 
 case "$CMD" in
-h*) show_help ;;
-remove | r*) uninstall_packages ;;
-list | l*) list_packages ;;
-add | i*) install_packages "$@" ;;
-*)
-    echo "Unknown command: '$CMD' (run 'pkg help' for usage information)"
-    exit 1
-    ;;
+	help) show_help;;
+	add) install_packages "$@";;
+	remove) uninstall_packages "$@";;
+	list) list_packages;;
+	*)
+		echo "Unknown command: '$CMD' (run 'pkg help' for usage information)"
+		exit 1
+		;;
 esac
