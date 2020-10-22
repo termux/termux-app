44f1c74b7bf0aba967b86fb11e9a76143053bec5
SealedJoy <sealedjoy@gmail.com>
2020-10-22 13:27:08 +0100

    .version sourcing removed

--- bootstrap/changes/tel/.tel/scripts/readconfigs.sh
+++ bootstrap/changes/tel/.tel/scripts/readconfigs.sh
@@ -2,8 +2,8 @@
 # Handles TEL's configs 
 
 # Version Info
-. ~/.tel/.version
-. ~/.tel/scripts/status_manager/.version
+#. ~/.tel/.version
+#. ~/.tel/scripts/status_manager/.version
 
 # Config files - these each source relevant user configs in ~/storage/shared/tel
 source <(cat ~/.tel/configs/*.*)
