157165ce776f77f053b0bc45c926e6d549515ea0
SealedJoy <sealedjoy@gmail.com>
2020-10-24 05:36:40 +0100

    newpipe opened if no youtube app with y

--- bootstrap/changes/tel/.tel/scripts/youtube.sh
+++ bootstrap/changes/tel/.tel/scripts/youtube.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #script for starting / searching youtube with one alias
 if [ "$#" == 0 ] ; then
-	tel-app youtube
+	tel-app youtube || tel-app newpipe
 else
 	search_term=$@
 	tel-search -yt "$search_term"
