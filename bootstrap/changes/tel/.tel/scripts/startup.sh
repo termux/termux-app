8e4c5a1566b35046f1988a6e0d699d3f03b66f65
SealedJoy <sealedjoy@gmail.com>
2020-10-25 01:58:16 +0100

    startup changes removing external storage

--- bootstrap/changes/tel/.tel/scripts/startup.sh
+++ bootstrap/changes/tel/.tel/scripts/startup.sh
@@ -29,7 +29,7 @@ fi
 if [ "$STARTUP_ANIMATION_ENABLED" == "true" ] ; then
 	echo -n 'launching animation                         ' $'\r'
 	sleep 0.1
-	tmux new-window -n 'Animation' 'python ~/.tel/scripts/animation.py'
+	tmux new-window -n 'skip with ctrl c' 'python ~/.tel/scripts/animation.py'
 	echo -ne "launched python animation ${CHECK_MARK}    " $'\r'
 fi
 
@@ -42,17 +42,15 @@ fi
 
 if [ $REBUILD_CACHES_STARTUP == "true" ]; then
 	echo -n "populating app cache..                       " $'\r'
-	timeout 15 ~/.tel/bin/tel-app -u || echo "Failed to build app cache"
+	timeout 15 ~/.tel/bin/tel-app -u || echo "Failed to build app cache, run 'app -u' to retry"
 	echo -ne "apps generated ${CHECK_MARK}                "  $'\r'
 	sleep 0.2
 
 	echo -n "populating contacts cache..                  " $'\r'
-	timeout 15 ~/.tel/bin/tel-dialer -u || echo "Failed to build contacts cache"
+	timeout 15 ~/.tel/bin/tel-dialer -u || echo "Failed to build contacts cache, run dialer -u to retry"
 	echo -ne "contacts generated ${CHECK_MARK}            " $'\r'
 	sleep 0.2
 fi
-# start user scripts
-sh /storage/emulated/0/tel/tel_startup.sh
 
 echo -ne "Ready!      ${CHECK_MARK}                " $'\r'
 sleep 0.2
