58932d20d58d6ce3cb0e6c1b0a2455424617e989
SealedJoy <sealedjoy@gmail.com>
2020-10-27 22:07:48 +0000

    setup fixes

--- bootstrap/changes/tel/.tel/extras/setup.sh
+++ bootstrap/changes/tel/.tel/extras/setup.sh
@@ -25,21 +25,22 @@ error() {
 	printf "\033[0;%sm%s\033[0m\033[0;%sm%s\033[0m\n" "${WHITE}" "[TEL]: " "${RED}" "${1}"
 }
 
-apt-get update -y && apt-get upgrade -y && logf "finished updating Termux packages" #print to screen as hotfix
-
+apt-get update -y #get latest pkg list 
+cd ~/.tel/extras
 # VIM SETUP
-echo 'Would you like to install VIM editor?'
+echo 'Would you like to install VIM editor? y/n'
 read userinput
 if [ $userinput == Y ] || [ $userinput == y ]  ; then
 catch "$(git clone https://github.com/sealedjoy/setup-vim 2>&1)"
 cd setup-vim
 chmod +x setup.sh
 ./setup.sh
+cd .. && rm -rf setup-vim
 fi
 
 # GAMES setup
-echo 'Would you like to install cli games?'
-echo 'includes: tetris, pacman, sudoku, snake, dungeon crawler, solitaire'
+log 'Would you like to install cli games?'
+log 'includes: tetris, pacman, sudoku, snake, dungeon crawler, solitaire'
 read userinput
 if [ $userinput == Y ] || [ $userinput == y ]  ; then
 catch "$(apt-get install vitetris nudoku nsnake nethack tty-solitaire myman -y 2>&1)"
@@ -50,12 +51,9 @@ read userinput
 if [ $userinput == Y ] || [ $userinput == y ]  ; then
 catch "$(apt-get install cmatrix -y 2>&1)"
 fi
-
-
-
-
 log "finished"
 error "app will restart in 3 seconds!"
 sleep 3
 tel-restart
-error 'Restart cannot be performed automatically when the TEL app is not active, Please run the command "tel-restart" manually'
+error 'Restart cannot be performed automatically when the TEL app is not active.
+error "Please run the command 'tel-restart' manually"
