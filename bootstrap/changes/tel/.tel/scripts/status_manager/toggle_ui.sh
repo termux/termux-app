15f447e74508fbd1bbeaec5cdf05456ec409c221
SealedJoy <sealedjoy@gmail.com>
2020-10-28 04:17:36 +0000

    fix duplicate problem when spawning status man

--- bootstrap/changes/tel/.tel/scripts/status_manager/toggle_ui.sh
+++ bootstrap/changes/tel/.tel/scripts/status_manager/toggle_ui.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 running=$(pgrep -f status_manager.py)
 if [ -z "$running" ] ; then
-	tmux new-window -n 'StatusManager' 'python ~/.tel/scripts/status_manager/status_manager.py'
+	#tmux new-window -n 'StatusManager' 'python ~/.tel/scripts/status_manager/status_manager.py'
+	tmux new-window 'python ~/.tel/scripts/status_manager/status_manager.py' # fix for duplicate naming error
 	tmux join-pane -d -b -s 'StatusManager' -t 1.top #for each pane trigger joining of status pane
 	tmux select-pane -t 1.top -T 'Status' -d	#disable input to status manager window
 	#need a way to lock pane height
