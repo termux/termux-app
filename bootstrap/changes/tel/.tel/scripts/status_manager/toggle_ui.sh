927ccb218d0e6e6e169d5137f0f4fc03bbf8c4e0
SealedJoy <sealedjoy@gmail.com>
2020-10-28 06:26:11 +0000

    lock pane size

--- bootstrap/changes/tel/.tel/scripts/status_manager/toggle_ui.sh
+++ bootstrap/changes/tel/.tel/scripts/status_manager/toggle_ui.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 running=$(pgrep -f status_manager.py)
 if [ -z "$running" ] ; then
-	# todo: fix not spawning on pane 1
+	# todo: link pane to display across all windows
 	tmux new-window -n 'StatusManager' 'python ~/.tel/scripts/status_manager/status_manager.py'
-	#tmux new-window 'python ~/.tel/scripts/status_manager/status_manager.py' # fix for duplicate naming error
 	tmux join-pane -d -b -s 'StatusManager' -t 1.top #for each pane trigger joining of status pane
-	tmux rename-window -t 1 $NAME
+	tmux rename-window -t 1 $NAME 			# rename after spawn for uniqueness
 	tmux select-pane -t 1.top -T 'Status' -d	#disable input to status manager window
-	#need a way to lock pane height
+	tmux set-window-option -t 1 aggressive-resize off  # attempt to lock pane height
 else
 	kill "$(pgrep -f 'status_manager.py')"
 	pkill -f 'termux-api' 
