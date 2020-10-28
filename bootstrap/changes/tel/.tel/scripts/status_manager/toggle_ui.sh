b569dc0e21c23fcd90381eb88a27d22d6cfdafd7
SealedJoy <sealedjoy@gmail.com>
2020-10-28 04:12:11 +0000

    disable user input to status manager

--- bootstrap/changes/tel/.tel/scripts/status_manager/toggle_ui.sh
+++ bootstrap/changes/tel/.tel/scripts/status_manager/toggle_ui.sh
@@ -2,7 +2,9 @@
 running=$(pgrep -f status_manager.py)
 if [ -z "$running" ] ; then
 	tmux new-window -n 'StatusManager' 'python ~/.tel/scripts/status_manager/status_manager.py'
-	tmux join-pane -d -b -s 'StatusManager' -t 1.top
+	tmux join-pane -d -b -s 'StatusManager' -t 1.top #for each pane trigger joining of status pane
+	tmux select-pane -t 1.top -T 'Status' -d	#disable input to status manager window
+	#need a way to lock pane height
 else
 	kill "$(pgrep -f 'status_manager.py')"
 	pkill -f 'termux-api' 
