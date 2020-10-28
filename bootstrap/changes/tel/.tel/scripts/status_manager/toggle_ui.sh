f008e509649df85f46170c8b87e80cc32ac4d126
SealedJoy <sealedjoy@gmail.com>
2020-10-28 06:21:07 +0000

    status ui fixes

--- bootstrap/changes/tel/.tel/scripts/status_manager/toggle_ui.sh
+++ bootstrap/changes/tel/.tel/scripts/status_manager/toggle_ui.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 running=$(pgrep -f status_manager.py)
 if [ -z "$running" ] ; then
-	#tmux new-window -n 'StatusManager' 'python ~/.tel/scripts/status_manager/status_manager.py'
-	tmux new-window 'python ~/.tel/scripts/status_manager/status_manager.py' # fix for duplicate naming error
+	# todo: fix not spawning on pane 1
+	tmux new-window -n 'StatusManager' 'python ~/.tel/scripts/status_manager/status_manager.py'
+	#tmux new-window 'python ~/.tel/scripts/status_manager/status_manager.py' # fix for duplicate naming error
 	tmux join-pane -d -b -s 'StatusManager' -t 1.top #for each pane trigger joining of status pane
+	tmux rename-window -t 1 $NAME
 	tmux select-pane -t 1.top -T 'Status' -d	#disable input to status manager window
 	#need a way to lock pane height
 else
