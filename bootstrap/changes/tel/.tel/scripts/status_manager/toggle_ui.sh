86b0b35d8eb47ad7a55857f4cf79d779adc84264
SealedJoy <sealedjoy@gmail.com>
2020-10-29 03:53:44 +0000

    shift order toggle ui

--- bootstrap/changes/tel/.tel/scripts/status_manager/toggle_ui.sh
+++ bootstrap/changes/tel/.tel/scripts/status_manager/toggle_ui.sh
@@ -4,9 +4,8 @@ if [ -z "$running" ] ; then
 	# todo: link pane to display across all windows
 	tmux new-window -n 'StatusManager' 'python ~/.tel/scripts/status_manager/status_manager.py'
 	tmux join-pane -d -b -s 'StatusManager' -t 1.top #for each pane trigger joining of status pane
-	tmux select-pane -t 1.top -d	#disable input to status manager window
 	tmux rename-window -t 1 $NAME 			# rename after spawn for uniqueness
-	tmux set-window-option -t 1 aggressive-resize off  # attempt to lock pane height
+	tmux select-pane -t $NAME.top -d	#disable input to status manager window
 else
 	kill "$(pgrep -f 'status_manager.py')"
 	pkill -f 'termux-api' 
