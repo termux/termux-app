1bf70c4a47847940a26d0ab5ff13b77497dc997f
SealedJoy <sealedjoy@gmail.com>
2020-11-01 15:36:16 +0000

    status fixes

--- bootstrap/changes/tel/.tel/scripts/status_manager/status_manager.py
+++ bootstrap/changes/tel/.tel/scripts/status_manager/status_manager.py
@@ -51,12 +51,12 @@ def print_status_bar(prev_status_bar,script_num,new_output):
             if new_output != prev_status_bar[line]:
                #replace
                 status_bar[line] = new_output
-                if len(term.strip_seqs(new_output)) > term.width:
+                if len(str(term.strip_seqs(new_output))) > term.width:
                     new_output = new_output[:term.width -8]
                     new_output = new_output + term.normal + '..'
-                    os.system("tel-status " + str(line) + " " + str(term.strip_seqs(new_output))
+                    os.system("tel-status " + str(line) + " " + str(term.strip_seqs(new_output)))
                 else:
-                    os.system("tel-status " + str(line) + " " + str(term.strip_seqs(new_output))
+                    os.system("tel-status " + str(line) + " " + str(term.strip_seqs(new_output)))
     return status_bar
 
 #read output from status script wrappers stdout, when line has been read, puts it into queue
