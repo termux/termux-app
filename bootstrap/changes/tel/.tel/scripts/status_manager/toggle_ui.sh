aed1c92e863b29f872d3cc0e7531b21138d290d3
SealedJoy <sealedjoy@gmail.com>
2020-11-01 16:22:06 +0000

    change toggle ui to new status dispaly method

--- bootstrap/changes/tel/.tel/scripts/status_manager/toggle_ui.sh
+++ bootstrap/changes/tel/.tel/scripts/status_manager/toggle_ui.sh
@@ -3,10 +3,11 @@
 running=$(pgrep -f status_manager.py)
 if [ -z "$running" ] ; then
 	# todo: link pane to display across all windows
-	tmux new-window -n 'StatusManager' 'python ~/.tel/scripts/status_manager/status_manager.py'
-	tmux join-pane -d -b -s 'StatusManager' -t 1.top #for each pane trigger joining of status pane
-	tmux rename-window -t 1 $NAME 			# rename after spawn for uniqueness
-	tmux select-pane -t $NAME.top -d	#disable input to status manager window
+	#tmux new-window -n 'StatusManager' 'python ~/.tel/scripts/status_manager/status_manager.py'
+	tmux run-shell -b 'python ~/.tel/scripts/status_manager/status_manager.py'
+	#tmux join-pane -d -b -s 'StatusManager' -t 1.top #for each pane trigger joining of status pane
+	#tmux rename-window -t 1 $NAME 			# rename after spawn for uniqueness
+	#tmux select-pane -t $NAME.top -d	#disable input to status manager window
 else
 	kill "$(pgrep -f 'status_manager.py')"
 	pkill -f 'termux-api' 
