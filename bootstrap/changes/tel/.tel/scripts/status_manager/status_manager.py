31c9e4b98d1e6fce41db988d7bc11c418fa5c89b
SealedJoy <sealedjoy@gmail.com>
2020-11-02 02:16:10 +0000

    fixes

--- bootstrap/changes/tel/.tel/scripts/status_manager/status_manager.py
+++ bootstrap/changes/tel/.tel/scripts/status_manager/status_manager.py
@@ -129,16 +129,14 @@ def main_loop():
 # end main_loop
 
 def clean_up():
-    status_bar = []
     for n in range(0,len(status_scripts)):
         os.system('tel-delete-status ' + str(n))
 
-
-
-try:
-    main_loop()
-except Exception as e:
-    print(e.message, e.args)
-finally:
-    clean_up()
-    exit()
+if __name__ == "__main__":
+    try:
+        main_loop()
+    except Exception as e:
+        print(e.message, e.args)
+    finally:
+        clean_up()
+        exit()
