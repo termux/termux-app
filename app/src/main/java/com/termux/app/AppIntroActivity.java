e2ea81b56f93c9df23b6bb5e5bb36f7724402ea4
v1nc <github@reckendrees.systems>
2021-01-10 16:58:13 +0100

    add empty intro

---
+++ app/src/main/java/com/termux/app/AppIntroActivity.java
@@ -0,0 +1,12 @@
+package com.termux.app;
+
+import android.os.Bundle;
+
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+
+public class AppIntroActivity extends IntroActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
