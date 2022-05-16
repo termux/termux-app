4795f45fb4af4a14617c8d5133d1bdfba0187028
cyj893 <cyj89317@gmail.com>
2022-05-16 09:54:43 +0900

    Changed: Replace Error Code with Exception
    Replaced Error Codes 0 with IllegalArgumentException by each case.

--- terminal-emulator/src/main/java/com/termux/terminal/TerminalColors.java
+++ terminal-emulator/src/main/java/com/termux/terminal/TerminalColors.java
@@ -32,45 +32,48 @@ public final class TerminalColors {
      * <p/>
      * Highest bit is set if successful, so return value is 0xFF${R}${G}${B}. Return 0 if failed.
      */
-    static int parse(String c) {
+    static int parse(String colorString) {
         try {
             int skipInitial, skipBetween;
-            if (c.charAt(0) == '#') {
+            if (colorString.charAt(0) == '#') {
                 // #RGB, #RRGGBB, #RRRGGGBBB or #RRRRGGGGBBBB. Most significant bits.
                 skipInitial = 1;
                 skipBetween = 0;
-            } else if (c.startsWith("rgb:")) {
+            } else if (colorString.startsWith("rgb:")) {
                 // rgb:<red>/<green>/<blue> where <red>, <green>, <blue> := h | hh | hhh | hhhh. Scaled.
                 skipInitial = 4;
                 skipBetween = 1;
             } else {
-                return 0;
+                throw new IllegalArgumentException("Wrong Prefix Format: '" + colorString + "'");
             }
-            int charsForColors = c.length() - skipInitial - 2 * skipBetween;
-            if (charsForColors % 3 != 0) return 0; // Unequal lengths.
+            int charsForColors = colorString.length() - skipInitial - 2 * skipBetween;
+            if (charsForColors % 3 != 0) {
+                throw new IllegalArgumentException("Unequal Length: '" + colorString + "'");
+            }
             int componentLength = charsForColors / 3;
             double mult = 255 / (Math.pow(2, componentLength * 4) - 1);
 
             int currentPosition = skipInitial;
-            String rString = c.substring(currentPosition, currentPosition + componentLength);
+            String rString = colorString.substring(currentPosition, currentPosition + componentLength);
             currentPosition += componentLength + skipBetween;
-            String gString = c.substring(currentPosition, currentPosition + componentLength);
+            String gString = colorString.substring(currentPosition, currentPosition + componentLength);
             currentPosition += componentLength + skipBetween;
-            String bString = c.substring(currentPosition, currentPosition + componentLength);
+            String bString = colorString.substring(currentPosition, currentPosition + componentLength);
 
             int r = (int) (Integer.parseInt(rString, 16) * mult);
             int g = (int) (Integer.parseInt(gString, 16) * mult);
             int b = (int) (Integer.parseInt(bString, 16) * mult);
             return 0xFF << 24 | r << 16 | g << 8 | b;
-        } catch (NumberFormatException | IndexOutOfBoundsException e) {
-            return 0;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("NumberFormatException: '" + colorString + "'");
+        } catch (IndexOutOfBoundsException e) {
+            throw new IllegalArgumentException("IndexOutOfBoundsException: '" + colorString + "'");
         }
     }
 
     /** Try parse a color from a text parameter and into a specified index. */
     public void tryParseColor(int intoIndex, String textParameter) {
-        int c = parse(textParameter);
-        if (c != 0) mCurrentColors[intoIndex] = c;
+        mCurrentColors[intoIndex] = parse(textParameter);
     }
 
 }
