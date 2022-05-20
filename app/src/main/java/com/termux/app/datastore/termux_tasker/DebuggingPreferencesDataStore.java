0fb4273618439cd6d76fd0f207d0f574500cf957
taehoonkange <sls97117@gmail.com>
2022-05-20 19:17:37 +0900

    refactor: extract method and change switch case to conditional operator
    Lines of code and McCabe Cyclomatic Complexity (CC) are reduced.

--- app/src/main/java/com/termux/app/datastore/termux_tasker/DebuggingPreferencesDataStore.java
+++ app/src/main/java/com/termux/app/datastore/termux_tasker/DebuggingPreferencesDataStore.java
@@ -31,31 +31,26 @@ public class DebuggingPreferencesDataStore extends PreferenceDataStore {
     @Override
     @Nullable
     public String getString(String key, @Nullable String defValue) {
-        if (mPreferences == null) return null;
-        if (key == null) return null;
-
-        switch (key) {
-            case "log_level":
-                return String.valueOf(mPreferences.getLogLevel(true));
-            default:
-                return null;
-        }
+        if (isDisability(key)) return null;
+        return (key =="log_level") ? String.valueOf(mPreferences.getLogLevel(true)) : null;
     }
 
     @Override
     public void putString(String key, @Nullable String value) {
-        if (mPreferences == null) return;
-        if (key == null) return;
-
-        switch (key) {
-            case "log_level":
-                if (value != null) {
-                    mPreferences.setLogLevel(mContext, Integer.parseInt(value), true);
-                }
-                break;
-            default:
-                break;
+        if (isDisability(key)) return;
+        if(key ==  "log_level") {
+            _put(value);
         }
+    }
+
+    private void _put(@Nullable String value) {
+        if (value != null) {
+            mPreferences.setLogLevel(mContext, Integer.parseInt(value), true);
+        }
+    }
+
+    private boolean isDisability(String key) {
+        return (mPreferences == null || key == null) ? true : false;
     }
 
 }
