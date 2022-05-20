33d8acfd3d9eb3408b8a665dd2bce51cb6726f48
taehoonkange <sls97117@gmail.com>
2022-05-20 18:11:49 +0900

    refactor: extract class `TerminalViewPreferencesDataStore.java`
    The result helps maintain adherence to the Single Responsibility Principle. The code of your classes will be more obvious and understandable.

---
+++ app/src/main/java/com/termux/app/datastore/termux/TerminalViewPreferencesDataStore.java
@@ -0,0 +1,57 @@
+package com.termux.app.datastore.termux;
+
+import android.content.Context;
+
+import androidx.preference.PreferenceDataStore;
+
+import com.termux.shared.termux.settings.preferences.TermuxAppSharedPreferences;
+
+public class TerminalViewPreferencesDataStore extends PreferenceDataStore {
+
+    private final Context mContext;
+    private final TermuxAppSharedPreferences mPreferences;
+
+    private static TerminalViewPreferencesDataStore mInstance;
+
+    private TerminalViewPreferencesDataStore(Context context) {
+        mContext = context;
+        mPreferences = TermuxAppSharedPreferences.build(context, true);
+    }
+
+    public static synchronized TerminalViewPreferencesDataStore getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new TerminalViewPreferencesDataStore(context);
+        }
+        return mInstance;
+    }
+
+
+
+    @Override
+    public void putBoolean(String key, boolean value) {
+        if (mPreferences == null) return;
+        if (key == null) return;
+
+        switch (key) {
+            case "terminal_margin_adjustment":
+                mPreferences.setTerminalMarginAdjustment(value);
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public boolean getBoolean(String key, boolean defValue) {
+        if (mPreferences == null) return false;
+
+        switch (key) {
+            case "terminal_margin_adjustment":
+                return mPreferences.isTerminalMarginAdjustmentEnabled();
+            default:
+                return false;
+        }
+    }
+
+}
+
