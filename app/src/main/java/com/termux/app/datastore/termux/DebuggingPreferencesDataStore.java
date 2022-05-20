d2a6fd0d6b9383ef9b4c4ecab8fd8d585f9e75a4
taehoonkange <sls97117@gmail.com>
2022-05-20 10:56:32 +0900

    refactor : extract method and change switch case to condtional operator
    The result that lines of code is reducecd and McCabe Cyclomatic Complexity (CC) is reduced

--- app/src/main/java/com/termux/app/datastore/termux/DebuggingPreferencesDataStore.java
+++ app/src/main/java/com/termux/app/datastore/termux/DebuggingPreferencesDataStore.java
@@ -31,39 +31,31 @@ public class DebuggingPreferencesDataStore extends PreferenceDataStore {
     @Override
     @Nullable
     public String getString(String key, @Nullable String defValue) {
-        if (mPreferences == null) return null;
-        if (key == null) return null;
+        if (isDisability(key)) return null;
+        return (key == "log_level") ? String.valueOf(mPreferences.getLogLevel()): null;
+    }
 
-        switch (key) {
-            case "log_level":
-                return String.valueOf(mPreferences.getLogLevel());
-            default:
-                return null;
-        }
+    private boolean isDisability(String key) {
+        return (mPreferences == null || key == null) ? true : false;
     }
 
     @Override
     public void putString(String key, @Nullable String value) {
-        if (mPreferences == null) return;
-        if (key == null) return;
+        if (isDisability(key)) return;
 
-        switch (key) {
-            case "log_level":
-                if (value != null) {
-                    mPreferences.setLogLevel(mContext, Integer.parseInt(value));
-                }
-                break;
-            default:
-                break;
-        }
+        if ( key == "log_level"  )
+            _put(value);
+
     }
 
+    private void _put(@Nullable String value) {
+        if (value != null) mPreferences.setLogLevel(mContext, Integer.parseInt(value));
+    }
 
 
     @Override
     public void putBoolean(String key, boolean value) {
-        if (mPreferences == null) return;
-        if (key == null) return;
+        if (isDisability(key)) return;
 
         switch (key) {
             case "terminal_view_key_logging_enabled":
