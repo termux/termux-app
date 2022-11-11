db0927c016dcb268e5e32e15319a11118b0c89b7
chedim <chedim@chedim.com>
2022-11-11 17:55:19 -0500

    Makes notification announcer respect the DND mode

--- app/src/main/java/com/termux/app/voice/NotificationAnnouncerService.java
+++ app/src/main/java/com/termux/app/voice/NotificationAnnouncerService.java
@@ -1,34 +1,65 @@
 package com.termux.app.voice;
 
 import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.provider.Settings;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.speech.tts.TextToSpeech;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Locale;
+import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class NotificationAnnouncerService extends NotificationListenerService implements TextToSpeech.OnInitListener {
 
     private static final Locale RU = new Locale("ru", "RU");
     private TextToSpeech tts;
 
+    private Set<String> ongoing = new HashSet<>();
+
     @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-        tts = new TextToSpeech(base, this);
+    public IBinder onBind(Intent intent) {
+        IBinder result = super.onBind(intent);
+        tts = new TextToSpeech(getBaseContext(), this);
+        return result;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        tts.stop();
+        tts = null;
+        return false;
     }
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
         super.onNotificationPosted(sbn);
         final Notification notification = sbn.getNotification();
-        final String title = notification.extras.getString("android.title");
+        try {
+            int filter = Settings.Global.getInt(getContentResolver(), "zen_mode");
+            if (notification.priority < filter) {
+                return;
+            }
+        } catch (Settings.SettingNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        final String title = sanitize(notification.extras.getString("android.title"));
         final String text = sanitize(notification.extras.getString("android.text"));
+        final String key = sbn.getKey();
+
         tts.setLanguage(detectLocale(title));
-        tts.speak(title, TextToSpeech.QUEUE_ADD, null, sbn.getKey());
+        tts.speak(title, TextToSpeech.QUEUE_ADD, null, key);
         tts.setLanguage(detectLocale(text));
-        tts.speak(text, TextToSpeech.QUEUE_ADD, null, sbn.getKey());
+        tts.speak(text, TextToSpeech.QUEUE_ADD, null, key);
     }
 
     @Override
@@ -50,6 +81,6 @@ public class NotificationAnnouncerService extends NotificationListenerService im
     }
 
     protected String sanitize(String text) {
-        return text.replaceAll("^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]", "[link]");
+        return text == null ? "" : text.replaceAll("^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]", "[link]");
     }
 }
