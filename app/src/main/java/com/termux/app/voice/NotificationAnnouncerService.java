0f3a9ace0afb571e2bd8d00d813dba405c7482d4
chedim <chedim@chedim.com>
2022-11-11 16:43:35 -0500

    Russian language support (yes, I receive notifications in Russian :)

--- app/src/main/java/com/termux/app/voice/NotificationAnnouncerService.java
+++ app/src/main/java/com/termux/app/voice/NotificationAnnouncerService.java
@@ -6,8 +6,11 @@ import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.speech.tts.TextToSpeech;
 
+import java.util.Locale;
+
 public class NotificationAnnouncerService extends NotificationListenerService implements TextToSpeech.OnInitListener {
 
+    private static final Locale RU = new Locale("ru", "RU");
     private TextToSpeech tts;
 
     @Override
@@ -19,9 +22,13 @@ public class NotificationAnnouncerService extends NotificationListenerService im
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
         super.onNotificationPosted(sbn);
-        Notification notification = sbn.getNotification();
-        tts.speak(notification.extras.getString("android.title"), TextToSpeech.QUEUE_ADD, null, sbn.getKey());
-        tts.speak(notification.extras.getString("android.text"), TextToSpeech.QUEUE_ADD, null, sbn.getKey());
+        final Notification notification = sbn.getNotification();
+        final String title = notification.extras.getString("android.title");
+        final String text = sanitize(notification.extras.getString("android.text"));
+        tts.setLanguage(detectLocale(title));
+        tts.speak(title, TextToSpeech.QUEUE_ADD, null, sbn.getKey());
+        tts.setLanguage(detectLocale(text));
+        tts.speak(text, TextToSpeech.QUEUE_ADD, null, sbn.getKey());
     }
 
     @Override
@@ -32,5 +39,17 @@ public class NotificationAnnouncerService extends NotificationListenerService im
     @Override
     public void onInit(int status) {
 
+    }
+
+    protected Locale detectLocale(String text) {
+        if (text.matches(".*[а-яА-Я].*")) {
+            return RU;
+        }
+
+        return Locale.ENGLISH;
+    }
+
+    protected String sanitize(String text) {
+        return text.replaceAll("^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]", "[link]");
     }
 }
