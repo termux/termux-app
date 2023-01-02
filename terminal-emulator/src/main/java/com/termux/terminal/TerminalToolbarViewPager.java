d901211d4a5e973f1a0824687100f0d84f76cdb6
github.com/sessions/verified-device/lostleolotus/profile/overview <lostleo86@gmail.com>
2023-01-02 08:39:50 -0600

    Add files via upload

---
+++ terminal-emulator/src/main/java/com/termux/terminal/TerminalToolbarViewPager.java
@@ -0,0 +1,117 @@
+package com.termux.app.terminal.io;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.termux.R;
+import com.termux.app.TermuxActivity;
+import com.termux.shared.termux.extrakeys.ExtraKeysView;
+import com.termux.terminal.TerminalSession;
+
+public class TerminalToolbarViewPager {
+
+    public static class PageAdapter extends PagerAdapter {
+
+        final TermuxActivity mActivity;
+        String mSavedTextInput;
+
+        public PageAdapter(TermuxActivity activity, String savedTextInput) {
+            this.mActivity = activity;
+            this.mSavedTextInput = savedTextInput;
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+            return view == object;
+        }
+
+        @NonNull
+        @Override
+        public Object instantiateItem(@NonNull ViewGroup collection, int position) {
+            LayoutInflater inflater = LayoutInflater.from(mActivity);
+            View layout;
+            if (position == 0) {
+                layout = inflater.inflate(R.layout.view_terminal_toolbar_extra_keys, collection, false);
+                ExtraKeysView extraKeysView = (ExtraKeysView) layout;
+                extraKeysView.setExtraKeysViewClient(mActivity.getTermuxTerminalExtraKeys());
+                extraKeysView.setButtonTextAllCaps(mActivity.getProperties().shouldExtraKeysTextBeAllCaps());
+                mActivity.setExtraKeysView(extraKeysView);
+                extraKeysView.reload(mActivity.getTermuxTerminalExtraKeys().getExtraKeysInfo(),
+                    mActivity.getTerminalToolbarDefaultHeight());
+
+                // apply extra keys fix if enabled in prefs
+                if (mActivity.getProperties().isUsingFullScreen() && mActivity.getProperties().isUsingFullScreenWorkAround()) {
+                    FullScreenWorkAround.apply(mActivity);
+                }
+
+            } else {
+                layout = inflater.inflate(R.layout.view_terminal_toolbar_text_input, collection, false);
+                final EditText editText = layout.findViewById(R.id.terminal_toolbar_text_input);
+
+                if (mSavedTextInput != null) {
+                    editText.setText(mSavedTextInput);
+                    mSavedTextInput = null;
+                }
+
+                editText.setOnEditorActionListener((v, actionId, event) -> {
+                    TerminalSession session = mActivity.getCurrentSession();
+                    if (session != null) {
+                        if (session.isRunning()) {
+                            String textToSend = editText.getText().toString();
+                            if (textToSend.length() == 0) textToSend = "\r";
+                            session.write(textToSend);
+                        } else {
+                            mActivity.getTermuxTerminalSessionClient().removeFinishedSession(session);
+                        }
+                        editText.setText("");
+                    }
+                    return true;
+                });
+            }
+            collection.addView(layout);
+            return layout;
+        }
+
+        @Override
+        public void destroyItem(@NonNull ViewGroup collection, int position, @NonNull Object view) {
+            collection.removeView((View) view);
+        }
+
+    }
+
+
+
+    public static class OnPageChangeListener extends ViewPager.SimpleOnPageChangeListener {
+
+        final TermuxActivity mActivity;
+        final ViewPager mTerminalToolbarViewPager;
+
+        public OnPageChangeListener(TermuxActivity activity, ViewPager viewPager) {
+            this.mActivity = activity;
+            this.mTerminalToolbarViewPager = viewPager;
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            if (position == 0) {
+                mActivity.getTerminalView().requestFocus();
+            } else {
+                final EditText editText = mTerminalToolbarViewPager.findViewById(R.id.terminal_toolbar_text_input);
+                if (editText != null) editText.requestFocus();
+            }
+        }
+
+    }
+
+}
---
+++ terminal-emulator/src/main/java/com/termux/terminal/TermuxTerminalExtraKeys.java
@@ -0,0 +1,108 @@
+package com.termux.app.terminal.io;
+
+import android.annotation.SuppressLint;
+import android.view.Gravity;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.termux.app.TermuxActivity;
+import com.termux.app.terminal.TermuxTerminalSessionActivityClient;
+import com.termux.app.terminal.TermuxTerminalViewClient;
+import com.termux.shared.logger.Logger;
+import com.termux.shared.termux.extrakeys.ExtraKeysConstants;
+import com.termux.shared.termux.extrakeys.ExtraKeysInfo;
+import com.termux.shared.termux.settings.properties.TermuxPropertyConstants;
+import com.termux.shared.termux.settings.properties.TermuxSharedProperties;
+import com.termux.shared.termux.terminal.io.TerminalExtraKeys;
+import com.termux.view.TerminalView;
+
+import org.json.JSONException;
+
+public class TermuxTerminalExtraKeys extends TerminalExtraKeys {
+
+    private ExtraKeysInfo mExtraKeysInfo;
+
+    final TermuxActivity mActivity;
+    final TermuxTerminalViewClient mTermuxTerminalViewClient;
+    final TermuxTerminalSessionActivityClient mTermuxTerminalSessionActivityClient;
+
+    private static final String LOG_TAG = "TermuxTerminalExtraKeys";
+
+    public TermuxTerminalExtraKeys(TermuxActivity activity, @NonNull TerminalView terminalView,
+                                   TermuxTerminalViewClient termuxTerminalViewClient,
+                                   TermuxTerminalSessionActivityClient termuxTerminalSessionActivityClient) {
+        super(terminalView);
+
+        mActivity = activity;
+        mTermuxTerminalViewClient = termuxTerminalViewClient;
+        mTermuxTerminalSessionActivityClient = termuxTerminalSessionActivityClient;
+
+        setExtraKeys();
+    }
+
+
+    /**
+     * Set the terminal extra keys and style.
+     */
+    private void setExtraKeys() {
+        mExtraKeysInfo = null;
+
+        try {
+            // The mMap stores the extra key and style string values while loading properties
+            // Check {@link #getExtraKeysInternalPropertyValueFromValue(String)} and
+            // {@link #getExtraKeysStyleInternalPropertyValueFromValue(String)}
+            String extrakeys = (String) mActivity.getProperties().getInternalPropertyValue(TermuxPropertyConstants.KEY_EXTRA_KEYS, true);
+            String extraKeysStyle = (String) mActivity.getProperties().getInternalPropertyValue(TermuxPropertyConstants.KEY_EXTRA_KEYS_STYLE, true);
+
+            ExtraKeysConstants.ExtraKeyDisplayMap extraKeyDisplayMap = ExtraKeysInfo.getCharDisplayMapForStyle(extraKeysStyle);
+            if (ExtraKeysConstants.EXTRA_KEY_DISPLAY_MAPS.DEFAULT_CHAR_DISPLAY.equals(extraKeyDisplayMap) && !TermuxPropertyConstants.DEFAULT_IVALUE_EXTRA_KEYS_STYLE.equals(extraKeysStyle)) {
+                Logger.logError(TermuxSharedProperties.LOG_TAG, "The style \"" + extraKeysStyle + "\" for the key \"" + TermuxPropertyConstants.KEY_EXTRA_KEYS_STYLE + "\" is invalid. Using default style instead.");
+                extraKeysStyle = TermuxPropertyConstants.DEFAULT_IVALUE_EXTRA_KEYS_STYLE;
+            }
+
+            mExtraKeysInfo = new ExtraKeysInfo(extrakeys, extraKeysStyle, ExtraKeysConstants.CONTROL_CHARS_ALIASES);
+        } catch (JSONException e) {
+            Logger.showToast(mActivity, "Could not load and set the \"" + TermuxPropertyConstants.KEY_EXTRA_KEYS + "\" property from the properties file: " + e.toString(), true);
+            Logger.logStackTraceWithMessage(LOG_TAG, "Could not load and set the \"" + TermuxPropertyConstants.KEY_EXTRA_KEYS + "\" property from the properties file: ", e);
+
+            try {
+                mExtraKeysInfo = new ExtraKeysInfo(TermuxPropertyConstants.DEFAULT_IVALUE_EXTRA_KEYS, TermuxPropertyConstants.DEFAULT_IVALUE_EXTRA_KEYS_STYLE, ExtraKeysConstants.CONTROL_CHARS_ALIASES);
+            } catch (JSONException e2) {
+                Logger.showToast(mActivity, "Can't create default extra keys",true);
+                Logger.logStackTraceWithMessage(LOG_TAG, "Could create default extra keys: ", e);
+                mExtraKeysInfo = null;
+            }
+        }
+    }
+
+    public ExtraKeysInfo getExtraKeysInfo() {
+        return mExtraKeysInfo;
+    }
+
+    @SuppressLint("RtlHardcoded")
+    @Override
+    public void onTerminalExtraKeyButtonClick(View view, String key, boolean ctrlDown, boolean altDown, boolean shiftDown, boolean fnDown) {
+        if ("KEYBOARD".equals(key)) {
+            if(mTermuxTerminalViewClient != null)
+                mTermuxTerminalViewClient.onToggleSoftKeyboardRequest();
+        } else if ("DRAWER".equals(key)) {
+            DrawerLayout drawerLayout = mTermuxTerminalViewClient.getActivity().getDrawer();
+            if (drawerLayout.isDrawerOpen(Gravity.LEFT))
+                drawerLayout.closeDrawer(Gravity.LEFT);
+            else
+                drawerLayout.openDrawer(Gravity.LEFT);
+        } else if ("PASTE".equals(key)) {
+            if(mTermuxTerminalSessionActivityClient != null)
+                mTermuxTerminalSessionActivityClient.onPasteTextFromClipboard(null);
+        }  else if ("SCROLL".equals(key)) {
+            TerminalView terminalView = mTermuxTerminalViewClient.getActivity().getTerminalView();
+            if (terminalView != null && terminalView.mEmulator != null)
+                terminalView.mEmulator.toggleAutoScrollDisabled();
+        } else {
+            super.onTerminalExtraKeyButtonClick(view, key, ctrlDown, altDown, shiftDown, fnDown);
+        }
+    }
+
+}
