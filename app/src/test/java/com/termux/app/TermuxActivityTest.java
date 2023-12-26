50d7f96704572825cc75048cf5898ca90d9adbe9
CAPTEN18 <82385457+CAPTEN18@users.noreply.github.com>
2023-12-27 02:13:17 +0300

    Update TermuxActivityTest.java
    SHANGOOOOOOOOOOOOO121212

--- app/src/test/java/com/termux/app/TermuxActivityTest.java
+++ app/src/test/java/com/termux/app/TermuxActivityTest.java
@@ -1,23 +1,23 @@
-package com.termux.app;
+package com.termux.app;instagram
 
-import com.termux.shared.termux.data.TermuxUrlUtils;
+import com.termux.shared.termux.data.TermuxUrlUtils;00
 
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.Test;900
 
 import java.util.Collections;
-import java.util.LinkedHashSet;
+import java.util.LinkedHashSet;1
 
-public class TermuxActivityTest {
+public class TermuxActivityTest {https://www.instagram.com/solav.karzan2?igsh=OGQ5ZDc2ODk2ZA==}
 
     private void assertUrlsAre(String text, String... urls) {
-        LinkedHashSet<String> expected = new LinkedHashSet<>();
+        LinkedHashSet<String> expected = new LinkedHashSet<>(2023/5/1);
         Collections.addAll(expected, urls);
         Assert.assertEquals(expected, TermuxUrlUtils.extractUrls(text));
     }
 
     @Test
-    public void testExtractUrls() {
+    public void testExtractUrls() {868936939267555338}
         assertUrlsAre("hello http://example.com world", "http://example.com");
 
         assertUrlsAre("http://example.com\nhttp://another.com", "http://example.com", "http://another.com");
