5b5e71949b60103a0c0aa77d4f5132382aa97a4b
Fredrik Fornwall <fredrik@fornwall.net>
2024-06-24 09:11:31 +0200

    Fixed: Improve handling of empty ';' SGR sequences
    Currently the Termux terminal emulator prints "HI" in red with:
    
    ```sh
    printf "\e[31;m HI \e[0m"
    ```
    
    This is not how other terminals (tested on xterm, gnome-terminal,
    alacritty and the mac built in terminal) handle it, since they parse
    ""\e[31;m" as "\e[31;0m", where the "0" resets the colors.
    
    This change aligns with other terminals, as well as improves performance
    by avoiding allocating a new int[] array for each byte processed by
    `parseArg()`, and most importantly simplifies things by removing the
    `mIsCSIStart` and `mLastCSIArg` state, preparing for supporting ':'
    separated sub parameters such as used in
    https://sw.kovidgoyal.net/kitty/underlines/

--- terminal-emulator/src/test/java/com/termux/terminal/TerminalTest.java
+++ terminal-emulator/src/test/java/com/termux/terminal/TerminalTest.java
@@ -163,6 +163,13 @@ public class TerminalTest extends TerminalTestCase {
         assertEquals(TextStyle.COLOR_INDEX_FOREGROUND, mTerminal.mForeColor);
         enterString("\033[31;;m");
         assertEquals(TextStyle.COLOR_INDEX_FOREGROUND, mTerminal.mForeColor);
+        enterString("\033[31;m");
+        assertEquals(TextStyle.COLOR_INDEX_FOREGROUND, mTerminal.mForeColor);
+        enterString("\033[31;;41m");
+        assertEquals(TextStyle.COLOR_INDEX_FOREGROUND, mTerminal.mForeColor);
+        assertEquals(1, mTerminal.mBackColor);
+        enterString("\033[0m");
+        assertEquals(TextStyle.COLOR_INDEX_BACKGROUND, mTerminal.mBackColor);
 
 		// 256 colors:
 		enterString("\033[38;5;119m");
@@ -178,9 +185,17 @@ public class TerminalTest extends TerminalTestCase {
 		assertEquals(129, mTerminal.mBackColor);
 
 		// Multiple parameters at once:
-		enterString("\033[38;5;178;48;5;179;m");
+		enterString("\033[38;5;178;48;5;179m");
 		assertEquals(178, mTerminal.mForeColor);
 		assertEquals(179, mTerminal.mBackColor);
+
+		// Omitted parameter means zero:
+		enterString("\033[38;5;m");
+		assertEquals(0, mTerminal.mForeColor);
+		assertEquals(179, mTerminal.mBackColor);
+		enterString("\033[48;5;m");
+		assertEquals(0, mTerminal.mForeColor);
+		assertEquals(0, mTerminal.mBackColor);
 
 		// 24 bit colors:
 		enterString(("\033[0m")); // Reset fg and bg colors.
@@ -203,6 +218,16 @@ public class TerminalTest extends TerminalTestCase {
 
 		// 24 bit colors, invalid input:
 		enterString("\033[38;2;300;127;2;48;2;1;300;254m");
+		assertEquals(expectedForeground, mTerminal.mForeColor);
+		assertEquals(expectedBackground, mTerminal.mBackColor);
+
+		// 24 bit colors, omitted parameter means zero:
+		enterString("\033[38;2;255;127;m");
+		expectedForeground = 0xff000000 | (255 << 16) | (127 << 8);
+		assertEquals(expectedForeground, mTerminal.mForeColor);
+		assertEquals(expectedBackground, mTerminal.mBackColor);
+		enterString("\033[38;2;123;;77m");
+		expectedForeground = 0xff000000 | (123 << 16) | 77;
 		assertEquals(expectedForeground, mTerminal.mForeColor);
 		assertEquals(expectedBackground, mTerminal.mBackColor);
 	}
